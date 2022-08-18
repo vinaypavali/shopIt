b02e4c1c237b8ccb02161377538e1379d3c600f9
Vinay Kumar <pavalivinay6@gmail.com>
2022-08-18 09:49:48 +0530

    home navbar

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './home.css';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    const handleRegister = ()=>{
+        navigate("/register")
+      }
+       const handleLogin = ()=>{
+        navigate("/login" )
+      }
+
+  return (
+    <>
+    <div className="navbar">
+        <div className="navContainer">
+            <span className='logo'>wheeler</span>
+            <div className="navItems">
+                <button className="navButton"  onClick={handleRegister}>Register</button>
+                <button className="navButton"  onClick={handleLogin}>Login</button>   
+            </div>
+        </div>
+         </div>
+    </>
+  )
+}
+
+export default Home
